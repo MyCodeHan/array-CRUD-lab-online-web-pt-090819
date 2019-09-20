bb97f3b8d4e9475f3ab955d5ad78eb075e530ba0
Hannah Harrington <hannahharrington@outlook.com>
2019-09-20 01:18:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array= ["wow", "I", "am", "really", "learning"]
+  family= ["mom", "dad", "bro1", "bro2", "me"]
 end
 
 def add_element_to_end_of_array(array, element)
